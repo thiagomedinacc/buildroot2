bcd25146fbb1b6c220efc4e9dd7dc7a947dd24a3
thiagomedinacc <thiago.medina@acad.pucrs.br>
2020-04-30 15:24:48 -0300

    Adiciona custom-scripts e termina tutorial 2.2

---
+++ custom-scripts/pre-build.sh
@@ -0,0 +1,4 @@
+#Compile the syscall_test.c
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-uclibc-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
